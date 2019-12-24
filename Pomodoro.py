16fe1f9b73e69205c3d996d75d907949a5c09927
2754 <47343861+2754github@users.noreply.github.com>
2019-12-25 00:09:15 +0900

    Update Pomodoro.py

--- Pomodoro.py
+++ Pomodoro.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import sys
 import math
+from time import sleep
 
 args=sys.argv
  
@@ -19,6 +20,10 @@ sysfont=pygame.font.SysFont(None,20)
 text1=sysfont.render("Work time is {0} minutes".format(WORK),True,(0,0,0))
 text2=sysfont.render("Break time is {0} minutes".format(BREAK),True,(0,0,0))
 text3=sysfont.render("EXIT",True,(0,0,0))
+
+pygame.mixer.init()
+alarm=pygame.mixer.Sound("alarm.wav")
+powerdown=pygame.mixer.Sound("powerdown.wav")
 
 WORK*=60
 BREAK*=60
@@ -40,14 +45,18 @@ while True:
 	screen.blit(text3,(260,80))
 
 	time_passed_seconds=clock.tick(60)/1000
+
 	S+=T*time_passed_seconds
 	if S<3/2*math.pi:
 		pygame.draw.arc(screen,(0,255,0),(0,0,100,100),math.pi/2,-S,10)
 		pygame.draw.arc(screen,(255,0,0),(10,10,80,80),0,2*math.pi,10)
+	elif 3/2*math.pi<S<3/2*math.pi+0.1:
+		alarm.play()
 	else:
 		s+=t*time_passed_seconds
 		pygame.draw.arc(screen,(255,0,0),(10,10,80,80),math.pi/2,-s,10)
 		if s>=3/2*math.pi:
+			alarm.play()
 			s=-math.pi/2
 			S=-math.pi/2
 
@@ -55,4 +64,6 @@ while True:
 	for event in pygame.event.get():
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if exit_button.collidepoint(event.pos):
+				powerdown.play()
+				sleep(1)
 				sys.exit()
